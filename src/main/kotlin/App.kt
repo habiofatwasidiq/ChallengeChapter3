387a3703cf6fe56ef21ccbfea3382da7e5e9fefd
habiofatwasidiq <habiofatwasidiq@gmail.com>
2022-09-05 22:11:38 +0700

    finishing input and result

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,4 +1,8 @@
+import utils.IOUtils
+import model.Player
+
 class App {
+    private val player = mutableListOf<Player>()
 
     private fun printHeader(){
         println("terminal:-terminal$")
@@ -7,7 +11,54 @@ class App {
         println("==========================")
     }
     private fun inputAndResult() {
+        println("Masukan pemain 1 : ")
+        val player1 = IOUtils.readString()
 
+        println("Masukan pemain 2 : ")
+        val player2 = IOUtils.readString()
+        val status: Boolean
+        player.add(Player(player1,player2))
+
+        if (player2 != null) {
+            if (player1.lowercase() == player2.lowercase()) {
+                println("Hasil : ")
+                println("DRAW!")
+            } else if (player1.lowercase() == "batu") {
+                if(player2.lowercase() == "gunting"){
+                    status = true
+                    result(status)
+                }else{
+                    status = false
+                    result(status)
+                }
+            } else if (player1.lowercase() == "gunting") {
+                if (player2.lowercase() == "kertas") {
+                    status = true
+                    result(status)
+                } else {
+                    status = false
+                    result(status)
+                }
+            } else if (player1.lowercase() == "kertas") {
+                if (player2.lowercase() == "batu") {
+                    status = true
+                    result(status)
+                } else {
+                    status = false
+                    result(status)
+                }
+            }
+        }
+    }
+    private fun result (status: Boolean, ) {
+
+        println ("Hasil : ")
+        if (status) {
+            println("Pemain 1 Menang!")
+        }
+        else {
+            println("Pemain 2 MENANG!")
+        }
     }
 
     fun run () {
