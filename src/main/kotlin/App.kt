f79eac1e1dae56e6a0ac6385437a0e260e3b00cc
habiofatwasidiq <habiofatwasidiq@gmail.com>
2022-09-05 21:25:36 +0700

    finishing Feature Print Header

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,9 +1,25 @@
 class App {
 
+    private fun printHeader(){
+        println("terminal:-terminal$")
+        println("==========================")
+        println("GAME SUIT TERMINAL VERSION")
+        println("==========================")
+    }
+
+    fun run () {
+        printHeader()
+
+    }
+
     companion object{
         @JvmStatic
         fun main(args: Array<String>) {
+            val app = App()
+            app.run()
 
         }
     }
+
+
 }
